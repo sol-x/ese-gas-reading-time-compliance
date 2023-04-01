e7be8e4c56b1246f4ebda0760fe9ac9d4b7bd0ca
Srikanth-PS <129586388+Srikanth-PS@users.noreply.github.com>
2023-04-01 14:25:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def check_gas_testing_time_compliance() -> bool:
     # Put your code here
-    return True
-
+    import pandas as pd
+    import numpy as np
+    
+    df1 = pd.read_csv('periodical_gas_reading.csv')
+    df2 = pd.read_csv('entrant_gas_reading.csv')
+    
+    if (df1['gas reading time'] > df2['entry time']) & (df1['gas reading time'] < df2['exit time']):
+        return True
 
 if __name__ == "__main__":
     if check_gas_testing_time_compliance():
